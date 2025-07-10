ce7ee458995362da33b11dcac58956d5a7273924
Bye <bye@byecorps.com>
2025-07-10 21:30:06 +0100

    oh god there's a horrible bug

--- PSMetadataLib/PS3/PS3BluRayDisc.cs
+++ PSMetadataLib/PS3/PS3BluRayDisc.cs
@@ -9,6 +9,8 @@ public class PS3BluRayDisc(string path) : IPS3Directory
 
     public List<IPS3Content> Scan()
     {
+        if (!System.IO.Directory.Exists(Path.Join(Directory, "PS3_DISC.SFB")))
+            return [];
         var sfbFile = new PS3DiscSFBFile(Path.Join(Directory, "PS3_DISC.SFB"));
         var locations = sfbFile.GetExistingSfoLocations(Directory);
 
